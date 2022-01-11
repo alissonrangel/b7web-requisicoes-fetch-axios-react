29eef747908d8740b00c0f060d057c98811504c9
alissonrangel <alisson.rangel@hotmail.com>
2022-01-11 20:37:05 -0300

    exercicio finalizado com o fetch

--- README.md
+++ README.md
@@ -1,70 +1,71 @@
-# Getting Started with Create React App
+# anotacoes
+- não colocar fetch() diretamente no useEffect
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+VERBO / METHOD = GET, POST, PUT, DELETE
+GET = Pegar alguma info
+POST = Inserir alguma informação do sistema
+PUT = Alterar alguma info do sist
+DELETE = 
 
-## Available Scripts
 
-In the project directory, you can run:
+URL / ENDPOINT = Endereço daquela API
+BASE = https://api.meusite.com
+ENDPOINT = BASE + COMANDO ESPECIFICO
+  POST https://api.meusite.com/carros
+  GET https://api.meusite.com/carros
+  GET https://api.meusite.com/carros?search=fer
+  GET https://api.meusite.com/carros/1
+  PUT https://api.meusite.com/carros/1
+  DELETE https://api.meusite.com/carros/1
 
-### `npm start`
+Corpo da requisição
+- price = 120000
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Cabeçalho da requisição
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
 
-### `npm test`
+UTILIZANDO TOKENS
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+POST carro - para adiionar um carro
+url = 'https://api.b7web.com.br/carros/api/carro';
+para permitir precisa adicionar um token no body da requisição ou adicionar
+o token no Authorization do headers
 
-### `npm run build`
+body: {
+  token: 'daldnad'
+}
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+let result = await fetch(url,{
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    });
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Salvando token e userName no localStorage, para guardar os dados.
 
-### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+Upload de arquivos
+faz um form com os campos que serão enviados
+faz um useState para cada campo
+mas para o campo de upload de img usa o useRef e não o useState
+let body = new FormData();
+body.append('brand', carBrand);
+body.append('name', carName);
+body.append('price', carPrice);
+body.append('year', carYear);
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+if (carImgField.current.files.length > 0) {
+  body.append('photo', carImgField.current.files[0]);
+}
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+let result = await fetch('https://api.b7web.com.br/carros/api/carro', {
+  method: 'POST',
+  headers: {
+    'Authorization': `Bearer ${token}`
+  },
+  body
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,294 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect, useRef} from 'react';
+
+import Lottie from 'react-lottie';
+
+import animationData from './assets/lottie/78259-loading.json';
+import api from './api';
+
+
 
 function App() {
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [userName, setUserName] = useState(localStorage.getItem('username'));
+
+  const carImgField = useRef();
+
+  const [carBrand, setCarBrand] = useState('');
+  const [carName, setCarName] = useState('');
+  const [carPrice, setCarPrice] = useState('');
+  const [carYear, setCarYear] = useState('');
+  
+
+  const [carros, setCarros] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [cadastro, setCadastro] = useState(true);
+  const [year, setYear] = useState('');
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const getCars = async () => {
+
+
+    setCarros([]);
+    setLoading(true);
+    const ano = document.getElementById('ano').value;
+    let url = 'https://api.b7web.com.br/carros/api/carros';
+    if ( ano !== ''){
+      url += `?ano=${ano}`;
+    }
+    
+    const cars = await fetch(url)
+                  .then( response => response.json() )
+                  .then( data => {
+                    if ( data.error === ''){
+                      return data.cars
+                    } else {
+                      alert(data.error);
+                    }
+                  })
+    console.log(cars);    
+    setCarros(cars);
+    setLoading(false);              
+  }
+
+  const google = () => {
+    fetch('https://www.google.com.br')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault(); //pra não faxer o envio do formulário
+    let url = '';
+    let body = {};
+
+    if (cadastro) {
+      url = 'https://api.b7web.com.br/carros/api/auth/login';
+      body = {
+        email: email,
+        password: senha
+      }
+    } else {
+      url = 'https://api.b7web.com.br/carros/api/auth/register';
+      body = {
+        name: nome,
+        email: email,
+        password: senha
+      }
+    }
+
+    console.log('body', body);
+    
+
+    let result = await fetch(url,{
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    let json = await result.json();
+
+    if ( json.error === ''){
+      if(cadastro){
+        localStorage.setItem('token', json.token);
+        localStorage.setItem('username', json.user.name);
+        setToken(json.token);
+        setUserName(json.user.name);
+      }else{
+        localStorage.setItem('token', json.token);
+        localStorage.setItem('username', json.user.name);
+        setToken(json.token);
+        setUserName(json.user.name);
+      }
+    } else {
+      alert(json.error);
+    }
+    console.log('RESULT', json);
+  }
+
+  const handleCadastro = (e) => {
+    if (e.target.value === 'Login') {
+      setCadastro(true);
+    } else {
+      setCadastro(false);
+    }
+  }
+
+  const handleLogout = () => {
+    setToken('');
+    setUserName('');
+    localStorage.setItem('token', '');
+    localStorage.setItem('username', '');
+  }
+
+  const handleAddCarSubmit = async(e) => {
+    e.preventDefault();
+
+    let body = new FormData();
+    body.append('brand', carBrand);
+    body.append('name', carName);
+    body.append('price', carPrice);
+    body.append('year', carYear);
+
+    if (carImgField.current.files.length > 0) {
+      body.append('photo', carImgField.current.files[0]);
+    }
+
+    let result = await fetch('https://api.b7web.com.br/carros/api/carro', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      body
+    });
+
+    const json = await result.json()
+
+    if (json.error){
+      console.log(json.error);
+    } else {
+      alert('Carro adicionado com sucesso!!!');
+      console.log('RESULT', json);      
+      getCars();
+      setCarBrand('');
+      setCarName('');
+      setCarYear('');
+      setCarPrice('');
+    }
+  }
+
+  useEffect(() => {
+    getCars();
+    //google();    
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      { !token &&
+        <div>
+          <input type='radio' value='Login' name='cadastro' checked={cadastro} onClick={handleCadastro} />Login<br/>
+          <input type='radio' value='Signup' name='cadastro' checked={!cadastro} onClick={handleCadastro} />Cadastro<br/>
+          <hr/>
+          { cadastro &&
+          <h2>Faça Login</h2>
+          }
+          { !cadastro &&
+            <h2>Faça Cadastro</h2>
+          }
+          <form onSubmit={handleLoginSubmit}>
+            { cadastro === false &&
+            <>  
+              <label>
+                Nome
+                <input type='text' name='name' onChange={(e) => setNome(e.target.value) } />
+              </label>
+              <br/>
+            </>
+            }
+            <label>
+              E-mail
+              <input type='email' name='email' onChange={(e) => setEmail(e.target.value) } />
+            </label>
+            <br/>
+            <label>
+              Senha
+              <input type='password' name='password' onChange={(e) => setSenha(e.target.value) } />
+            </label>
+            <br/>
+            <input type='submit' value='Enviar'/>
+          </form>
+        </div>
+      }
+      { token &&
+        <div>
+          <h3>Olá, {userName}</h3>
+          <button onClick={handleLogout}>Sair</button>
+          <form onSubmit={handleAddCarSubmit}>
+            <label>
+              Marca do carro 
+              <input type='text' name='carbrand' value={carBrand} onChange={(e) => setCarBrand(e.target.value) } />
+            </label>
+            <br/>
+           
+            <label>
+              Nome do carro
+              <input type='text' name='carname' value={carName} onChange={(e) => setCarName(e.target.value) } />
+            </label>
+            <br/>
+            <label>
+              Ano do carro
+              <input type='text' name='caryear' value={carYear} onChange={(e) => setCarYear(e.target.value) } />
+            </label>
+            <br/>
+            <label>
+              Preço do carro
+              <input type='text' name='carprice' value={carPrice} onChange={(e) => setCarPrice(e.target.value) } />
+            </label>
+            <br/>
+            <label>
+              Foto do carro
+              <input ref={carImgField} type='file' name='carimg'   />
+
+            </label>
+            <br/>
+            <input type='submit' value='Enviar'/>
+          </form>
+        </div>
+      }
+      <hr/>
+
+      <h1>Lista de Carros</h1>
+      <select id='ano' onChange={getCars} >
+        <option value=''></option>
+        <option>2021</option>
+        <option>2020</option>
+        <option>2019</option>
+        <option>2018</option>
+        <option>2017</option>
+        <option>2016</option>
+        <option>2015</option>
+        <option>2014</option>        
+        <option>2013</option>
+        <option>2012</option>
+        <option>2011</option>
+        <option>2010</option>
+        <option>2009</option>
+        <option>2008</option>        
+      </select>
+      <button onClick={()=>getCars()}>Atualizar Lista</button>
+      <hr />
+      {
+        loading &&
+        <Lottie options={defaultOptions} width='400px' />
+      }
+      { carros.length == 0 && loading === false &&
+          <h2>Não há carros neste ano!!!</h2>
+      }
+      { carros.map( (item, index) => (
+        <div key={index}>
+          <img src={item.photo} />
+          <h3>{item.brand} - {item.name}</h3>
+          <p>R$ {item.price}</p>
+        </div>
+      ) ) }
+      
+    </>
   );
 }
 
